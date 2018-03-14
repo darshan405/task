2911b25ef1348f491edb06df3665d5cf1752d7dc
kmbhuvanprasad <dshivakumar@qwinix.io>
2018-03-14 17:40:23 +0530

    first commit on javascript task

---
+++ js/index2.js
@@ -0,0 +1,24 @@
+    function myFunction1() {
+    var x = document.getElementById("qwerty1");
+    if (x.style.background === "red") {
+    x.style.background = "white";
+    } else {
+    x.style.background = "red";
+    }
+    }
+    function myFunction2() {
+    var x = document.getElementById("qwerty2");
+    if (x.style.background === "blue") {
+    x.style.background = "white";
+    } else {
+    x.style.background = "blue";
+    }
+    }
+    function myFunction3() {
+    var x = document.getElementById("qwerty3");
+    if (x.style.background === "yellow") {
+    x.style.background = "white";
+    } else {
+    x.style.background = "yellow";
+    }
+    }
