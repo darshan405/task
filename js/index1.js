9d2a28f97e4940b2ae69a6ed49908a41fd653eb3
kmbhuvanprasad <dshivakumar@qwinix.io>
2018-03-15 17:31:41 +0530

    commit on form javascript task

--- js/index1.js
+++ js/index1.js
@@ -1,12 +1,20 @@
-	function validateForm() {
-	var x = document.forms["myForm"]["fname"].value;
-	if (x == "") {
-	alert("Name must be filled out");
-	return false;
+function validateForm() {
+	var ex1=document.getElementById("ex1").value;
+	var ex2=document.getElementById("ex2").value;
+	var ex3=document.getElementById("ex3").value;
+	var ex4=document.getElementById("ex4").value;
+	console.log(ex1)
+		debugger
+	if(ex1 == "" || ex2 == "" || ex3 == "" || ex4 == ""){
+		alert("Name must be filled out");
 	}
-	}
+	else{
+		document.getElementById("dispname1").innerHTML = ex1;
+		document.getElementById("dispname2").innerHTML = ex2;
+		document.getElementById("dispname3").innerHTML = ex3;
+		document.getElementById("dispname4").innerHTML = ex4;
+		document.getElementById('detail').style.display='block';
+		document.getElementById('detailfrm').style.display='none';
 
-	function disp(){
-		// body...
 	}
-	
+}
